35b5bfdf80ff50827f10a8af684ccf06ce5b09c7
Nam's_Desktop <vbnb0123@naver.com>
2023-09-08 19:17:37 +0900

    no message

--- Heap/Heap/Heap.h
+++ Heap/Heap/Heap.h
@@ -1,10 +1,121 @@
 #pragma once
 #include <iostream>
+#include <string>
+#include <sstream>
 
 using namespace std;
 
 template<typename T>
 class HeapTree
 {
+public:
+	struct HeapNode;
 
+public:
+	static HeapNode* Create(T** InData)
+	{
+		HeapNode* node = new HeapNode();
+		node->Data = *InData;
+		node->Left = NULL;
+		node->Right = NULL;
+
+		return node;
+	}
+
+public:
+	void InsertNode(HeapNode* InNode)
+	{
+
+	}
+
+	void PopNode()
+	{
+		if (m_RootNode == NULL)
+		{
+			return;
+		}
+	}
+
+	void RemoveAllNode()
+	{
+		if (m_RootNode == NULL)
+		{
+			return;
+		}
+	}
+
+private:
+
+private:
+	int IntDataOrder(T* RootData, T* InsertData)
+	{
+		if (*RootData < *InsertData)
+		{
+			return -1;
+		}
+		else if (*RootData > *InsertData)
+		{
+			return 1;
+		}
+
+		return 0;
+	}
+
+	int StringDataOrder(T* RootData, T* InsertData)
+	{
+		stringstream RootString;
+		stringstream InsertString;
+
+		RootString << *RootData;
+		InsertString << *InsertData;
+
+		if (RootString.str().compare(InsertString.str()) > 0)
+		{
+			return 1;
+		}
+		else if (RootString.str().compare(InsertString.str()) < 0)
+		{
+			return -1;
+		}
+
+		return 0;
+	}
+
+	int GetDataOrder(HeapNode* InRoot, T* InData)
+	{
+		string name = typeid(*InData).name();
+		int result = -99999;
+		if (name.find("int") != string::npos)
+		{
+			result = IntDataOrder(InRoot->Data, InData);
+		}
+		else if (name.find("string") != string::npos)
+		{
+			result = StringDataOrder(InRoot->Data, InData);
+		}
+
+		return result;
+	}
+
+public:
+	struct HeapNode
+	{
+		T* Data;
+		HeapNode* Left;
+		HeapNode* Right;
+	};
+
+	struct ListNode
+	{
+		HeapNode* Node;
+		ListNode* NextNode;
+	};
+
+private:
+	HeapNode* m_RootNode = NULL;
+	HeapNode* m_LastNode = NULL;
+
+private:
+	ListNode* m_ParentNodeList = NULL;
+	ListNode* m_LastNodeList = NULL;
 };
