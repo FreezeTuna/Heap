34d1c1c91542fd97b06fcc9c385e657a13bcd140
Nam's_Desktop <vbnb0123@naver.com>
2023-09-11 19:01:57 +0900

    no message

--- Heap/Heap/Heap.h
+++ Heap/Heap/Heap.h
@@ -10,6 +10,7 @@ class HeapTree
 {
 public:
 	struct HeapNode;
+	struct ListNode;
 
 public:
 	static HeapNode* Create(T** InData)
@@ -25,7 +26,23 @@ public:
 public:
 	void InsertNode(HeapNode* InNode)
 	{
+		if (m_RootNode == NULL)
+		{
+			m_RootNode = InNode;
+			InsertList(InNode, m_LastNodeList);
+			return;
+		}
 
+		HeapNode* lastNode = NULL;
+		int result = GetInsertLocation(lastNode);
+		if (result == -1)
+		{
+			lastNode->Left = InNode;
+		}
+		else if (result == 1)
+		{
+			lastNode->Right = InNode;
+		}
 	}
 
 	void PopNode()
@@ -45,11 +62,86 @@ public:
 	}
 
 private:
+	void InsertList(HeapNode* InHeapNode, ListNode* InListNode = NULL)
+	{
+		if (InListNode == NULL)
+		{
+			ListNode* listNode = new ListNode();
+			listNode->Node = InHeapNode;
+			listNode->Next = NULL;
+			InListNode = listNode;
+
+			return;
+		}
+
+		if (InListNode->Next == NULL)
+		{
+			ListNode* listNode = new ListNode();
+			listNode->Node = InHeapNode;
+			listNode->Next = NULL;
+			InListNode->Next = listNode;
+			return;
+		}
+		else
+		{
+			InsertList(InHeapNode, InListNode->Next);
+		}
+	}
+
+	int GetInsertLocation(HeapNode& InNode)
+	{
+		if (m_ParentNodeList == NULL)
+		{
+			m_ParentNodeList = m_LastNodeList;
+			m_LastNodeList = NULL;
+		}
+
+		if (m_ParentNodeList != NULL)
+		{
+			if (m_ParentNodeList->Node->Left == NULL)
+			{
+				InNode = m_ParentNodeList->Node;
+				return -1;
+			}
+			else if (m_ParentNodeList->Node->Right == NULL)
+			{
+				InNode = m_ParentNodeList->Node;
+				return 1;
+			}
+			else
+			{
+				m_ParentNodeList = m_ParentNodeList->Next;
+				GetInsertLocation(InNode);
+			}
+		}
+
+		return -99;
+	}
+
+	HeapNode* GetLastHeapNode()
+	{
+		if (m_LastNodeList == NULL)
+		{
+			return NULL;
+		}
+
+		ListNode* listNode = m_LastNodeList;
+		while (listNode->Next != NULL)
+		{
+			listNode = listNode->Next;
+		}
+
+		return listNode->Node;
+	}
 
 private:
 	int IntDataOrder(T* RootData, T* InsertData)
 	{
-		if (*RootData < *InsertData)
+		if (*RootData == *InsertData)
+		{
+			return 0;
+		}
+		else if (*RootData < *InsertData)
 		{
 			return -1;
 		}
@@ -57,8 +149,6 @@ private:
 		{
 			return 1;
 		}
-
-		return 0;
 	}
 
 	int StringDataOrder(T* RootData, T* InsertData)
@@ -113,7 +203,6 @@ public:
 
 private:
 	HeapNode* m_RootNode = NULL;
-	HeapNode* m_LastNode = NULL;
 
 private:
 	ListNode* m_ParentNodeList = NULL;
---
+++ README.txt
@@ -0,0 +1,12 @@
+---- Visual Studio 2017 Project ----
+
+
+---- 	Linked List Heap for C++ 	----
+
+Linked List 를 이용한 Heap Tree 제작
+
+2023.09.08
+기본적인 구조 및 함수들 작성
+
+2023.09.11
+Insert / Pop up 함수에 필요한 Linked List 함수들 작성
